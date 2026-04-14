c80ef2e9e880a3e6f4b255cafc97f5ae3fb87744
lewardnavy-lsu <lewardnavy@gmail.com>
2026-04-14 16:19:09 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -1,5 +1,5 @@
 var map = L.map('map').setView([38, -95], 4);
-var basemapUrl = 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}.png';
+var basemapUrl = 'https://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}';
 var basemap = L.tileLayer(basemapUrl).addTo(map);
 
 var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
@@ -16,7 +16,7 @@ $.getJSON(weatherAlertsUrl, function(data) {
      style: function(feature){
        var alertColor = 'orange';
        if (feature.properties.severity === 'Minor') alertColor = 'yellow';
-       if (feature.properties.severity === 'Severe') alertColor = 'red';
+       //if (feature.properties.severity === 'Severe') alertColor = 'red';
        if (feature.properties.severity === 'Extreme') alertColor = 'blue';
           return { color: alertColor };
     },
